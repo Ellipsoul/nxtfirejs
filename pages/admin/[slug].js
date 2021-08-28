6da2c8ecd938b70ed1f048294aa2c51e01afd2a8
Aron Teh <ewt17@ic.ac.uk>
2021-08-28 13:19:41 +0100

    Next js routing setup

---
+++ pages/admin/[slug].js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Dynamic route which will act as a variable
+export default function AdminPostEdit() {
+    return (
+        <main>
+            <h1>Edit Posts</h1>
+        </main>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+// This will be the root directory
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Hi
+    <div>
+      {/* Dynamic links with next js */}
+      <Link 
+        prefetch={false} 
+        href={{
+          pathname: '/[username]',
+          query: { username: 'ellipsoul'}
+        }}
+      >
+        <a>Arons Profile</a>
+      </Link> 
     </div>
   )
 }
