428834dda20fa66466e165b3c898448c4f498f88
Aron Teh <ewt17@ic.ac.uk>
2021-08-28 21:55:49 +0100

    Temp push, something not working with user context with enter.js

--- pages/enter.js
+++ pages/enter.js
@@ -1,14 +1,18 @@
 import Image from 'next/image';
-import React, { createContext } from 'react'
+import React, { useContext, useCallback, useEffect, useState } from 'react'
 import { auth, googleAuthProvider } from '../lib/firebase';
 import { signInWithPopup } from 'firebase/auth';
+import { doc } from "firebase/firestore";
+
+import debounce from 'lodash.debounce';
 
 import googleIcon from '../public/google.png';
 import { UserContext } from '../lib/context';
 
 export default function EnterPage() {
-    const {user, username} = createContext(UserContext);
-
+    const { user, username} = useContext(UserContext);
+    console.log(user);
+    console.log(username);
     // Main nav bar
     return (
         <main>
@@ -44,9 +48,103 @@ function SignOutButton() {
 
 // Form for user to select a username
 function UsernameForm() {
-    return (
-        <main>
-            <h1>Username Form</h1>
-        </main>
+    const [formValue, setFormValue] = useState('');  // Value user is typingi 
+    const [isValid, setIsValid] = useState(false);   // Whether the username is free
+    const [loading, setLoading] = useState(false)    // True when checking for the username
+
+    const { user, username } = useContext(UserContext);  // Grab the user and username
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+    
+        // Create refs for both documents
+        const userDoc = firestore.doc(`users/${user.uid}`);
+        const usernameDoc = firestore.doc(`usernames/${formValue}`);
+    
+        // Commit both docs together as a batch write.
+        const batch = firestore.batch();
+        batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName });
+        batch.set(usernameDoc, { uid: user.uid });
+    
+        await batch.commit();
+    };
+
+    // Check validity of username whenever it changes
+    useEffect(() => {
+        checkUsername(formValue);
+    }, [formValue, checkUsername]);
+
+    // Hit the database for username match after each debounced change
+    // useCallback is required for debounce to work
+    const checkUsername = useCallback(
+        debounce(async (username) => {
+            if (username.length >= 3) {  // Only check for sufficiently long usernames
+                const ref = doc(firestore, 'users', username); // Create reference to user document in firestore
+                const { exists } = await ref.get();
+                console.log('Firestore read executed!');
+                setIsValid(!exists);
+                setLoading(false);
+            }
+        }, 500),
+        []
     );
+
+    // Track changes in the form input
+    const onChange = (e) => {
+        const val = e.target.value.toLowerCase();  // Grab form value
+        const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;  // Only allow valid characters
+
+        // Name still too short
+        if (val.length < 3) {
+            setFormValue(val);
+            setLoading(false);
+            setIsValid(false);
+        }
+
+        // Name passes the regex test, start checking
+        if (re.test(val)) {
+            setFormValue(val);
+            setLoading(true);
+            setIsValid(false);
+        }
+
+    }
+
+    return (
+        !username && (
+            <section>
+                <h3>Choose Username</h3>
+                <form onSubmit={onSubmit}>
+                <input name="username" placeholder="myname" value={formValue} onChange={onChange} />
+                <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+                <button type="submit" className="btn-green" disabled={!isValid}>
+                    Choose
+                </button>
+        
+                {/* To observe username checking behaviour */}
+                <h3>Debug State</h3>
+                <div>
+                    Username: {formValue}
+                    <br />
+                    Loading: {loading.toString()}
+                    <br />
+                    Username Valid: {isValid.toString()}
+                </div>
+                </form>
+            </section>
+        )
+    );
+}
+
+
+function UsernameMessage({ username, isValid, loading }) {
+    if (loading) {
+        return <p>Checking...</p>;
+    } else if (isValid) {
+        return <p className="text-success">{username} is available!</p>;
+    } else if (username && !isValid) {
+        return <p className="text-danger">That username is taken!</p>;
+    } else {
+        return <p></p>;
+    }
 }
