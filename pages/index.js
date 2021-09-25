3ceb6983dbc6948f5734764f0a132efa53500c6b
Aron Teh <aronteh.chess@gmail.com>
2021-09-25 10:41:23 +0100

    Typo found! Collection group successfully imported. Home page is now working

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,43 @@
-import { useState } from 'react';
-import { firestore, fromMillis, postToJSON } from '../lib/firebase';
-import { where, orderBy, limit, query, getDocs, collectionGroup } from 'firebsae/firestore';
+import { useState } from "react";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
+import {
+  where,
+  orderBy,
+  limit,
+  query,
+  getDocs,
+  collectionGroup,
+  startAfter,
+} from "firebase/firestore";
 
-import PostFeed from '../components/PostFeed';
-import Loader from '../components/Loader';
+import PostFeed from "../components/PostFeed";
+import Loader from "../components/Loader";
 
 /*
 Root directory containing a list of user posts
 Server-side render 10 most recent posts, then request client side paginated query for 5 new posts at a time
 */
 
-const LIMIT = 1  // Number of posts for each batch
+const LIMIT = 1; // Number of posts for each batch
 
 export async function getServerSideProps(context) {
   // Grab all published posts from all users
-  // TODO: Currently collectionGroup is not supported in modular implementation
   const allPostsCollection = collectionGroup(firestore, "posts");
 
   // Query for all posts from all users
-  const postsQuery = query(allPostsCollection.ref,
-    where("published", "==", true),
-    orderBy('createdAt', 'desc'),
+  const postsQuery = query(
+    allPostsCollection,
+    where("published", "==", true), // Only display published posts
+    orderBy("createdAt", "desc"), // Reverse chronological order
     limit(LIMIT)
-  )
-  const querySnapshot = await getDocs(postsQuery);  // Execute query and grab a snapshot
+  );
+  const querySnapshot = await getDocs(postsQuery); // Execute query and grab a snapshot
 
-  // Process new posts (do this later)
+  // Process new posts and append each to an array of posts
+  let posts = [];
   querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+    posts.push(postToJSON(doc));
   });
-
-  // Namespaced implementation 
-  // const postsQuery = firestore
-  //   .collectionGroup('posts')
-  //   .where('published', '==', true)
-  //   .orderBy('createdAt', 'desc')
-  //   .limit(LIMIT)
-  
-  // const posts = (await postsQuery.get()).docs.map(postToJSON)  // Execute the query and map it to posts
 
   return {
     props: { posts },
@@ -45,48 +45,53 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home(props) {
-
-  const [posts, setPosts] = useState(props.posts)    // Track state of all posts
-  const [loading, isLoading] = useState(false);      // Track whether new posts are loading in
-  const [postsEnd, setPostsEnd] = useState(false);   // Track whether all posts have been loaded
+  const [posts, setPosts] = useState(props.posts);  // Track state of all posts
+  const [loading, isLoading] = useState(false);     // Track whether new posts are loading in
+  const [postsEnd, setPostsEnd] = useState(false);  // Track whether all posts have been loaded
 
   // Grab more user posts on request
   const getMorePosts = async () => {
-    isLoading(true);  // Acknowledge loading
+    isLoading(true); // Acknowledge loading
 
-    const last = posts[posts.length - 1]  // Grab last post
+    const last = posts[posts.length - 1]; // Grab last post
 
     // Take the timestamp of the last post, depending on whether it was retrieved client or server side
-    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+    const cursor =
+      typeof last.createdAt === "number" ? fromMillis(last.createdAt) : last.createdAt;
 
-    // TODO: Wait for a solution or wait for Google to update its collection group modular syntax!
-    // This is currently in namespaced syntax
-    const query = firestore
-      .collectionGroup('posts')
-      .where('published', '==', true)
-      .orderBy('createdAt', 'desc')
-      .startAfter(cursor)
-      .limit(LIMIT);
+    // Grab new posts
+    const allPostsCollection = collectionGroup(firestore, "posts");
+    const morePostsQuery = query(
+      allPostsCollection,
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(LIMIT)
+    );
 
-    // Excute the query
-    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+    // Excute the query and append to an array of new posts
+    let newPosts = [];
+    const newPostsQuerySnapshot = await getDocs(morePostsQuery);
+    newPostsQuerySnapshot.forEach((doc) => {
+      newPosts.push(doc.data())
+    })
+    setPosts(posts.concat(newPosts)); // Append the new posts to the current list
 
-    setPosts(posts.concat(newPosts));  // Append the new posts to the current list
-    setLoading(false);                 // Stop the loading bar
-
+    isLoading(false);                                    // Stop the loading bar
     if (newPosts.length < LIMIT) { setPostsEnd(true); }  // Reached the end of all posts
-  }
-
+  };
 
   return (
     <main>
       {/* Post Feed of all posts */}
       <PostFeed posts={posts} />
       {/* Option to load more posts if there are more */}
-      {!loading && !postsEnd ? <button onClick={getMorePosts}>Load More!</button> : null}
+      {!loading && !postsEnd ? (
+        <button onClick={getMorePosts}>Load More!</button>
+      ) : null}
       <Loader show={loading} />
       {/* Inform user if end of posts list has been reached */}
       {postsEnd ? <div>You have reached the end!</div> : null}
     </main>
-  )
+  );
 }
