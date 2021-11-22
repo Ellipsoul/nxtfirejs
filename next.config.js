bcbbec82400dc9ebf30aaf70d7b884835689796b
Aron Teh <aronteh.chess@gmail.com>
2021-11-22 23:43:17 +0000

    Randomly trying to add firestore to the next config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withTM = require('next-transpile-modules')(['react-markdown']);
+const withFB = require('firebase/firestore');
 
-module.exports = withTM({
+module.exports = withFB(withTM({
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-})
+}));
