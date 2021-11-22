3a11064384310443e947802b0f36b4d99ec75222
Aron Teh <aronteh.chess@gmail.com>
2021-11-22 23:49:53 +0000

    Another try

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['react-markdown', 'firebase/firestore']);
+const withTM = require('next-transpile-modules')(['react-markdown', '@firebase/firestore']);
 
 module.exports = withTM({
   reactStrictMode: true,
