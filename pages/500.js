d0c84cd5ef54b3827ee271403dfcd77b03b57e16
Aron Teh <aronteh.chess@gmail.com>
2021-11-23 00:16:08 +0000

    Goodbye :crying-face:

--- pages/500.js
+++ pages/500.js
@@ -1,6 +1,3 @@
-import Link from 'next/link';
-import toast from 'react-hot-toast'; 
-
 import Metatags from '../components/Metatags.js';
 
 // Custom 404 Page. Next automatically routes 404 errors to a file with this name
@@ -13,13 +10,13 @@ export default function Custom500() {
       <h1>500 Error</h1>
       <h2>
         One day I returned to my deployment in horror that it now returns a 500 error on the home page.
-        I tried everything to rescue my creation, but it will just have to be a part of history. RIP
+        I tried everything to rescue my creation, but it will just have to be a part of history. RIP 😢
       </h2>
       <h4>
         Based on my investigation, I believe the recent NextJS 12 update changed some deployment settings
         under the hood, causing some modules (most likely firestore) to not be imported properly for server
-        side rendering. I managed to import inside next.config.js, but that returned a very strange type
-        casting issue that I could not find anywhere on Google.
+        side rendering. I managed to import firestore inside next.config.js, but that returned a very strange
+        type casting issue that I could not find anywhere on Google. Goodbye old friend 🏳
       </h4>
 
       <iframe
@@ -29,16 +26,6 @@ export default function Custom500() {
         frameBorder="0"
         allowFullScreen
       ></iframe>
-      
-      <div className="buttons-404">
-        <Link href="/" passHref>
-          <button className="btn-blue">Home</button>
-        </Link>
-
-        <Link href="/" passHref>
-          <button className="btn-red" onClick={() => toast.success('Developer Fired')}>Return Home and Fire Developer</button>
-        </Link>
-      </div>
     </main>
   );
 }
